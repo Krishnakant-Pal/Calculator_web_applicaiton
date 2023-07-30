6e1b2f29df990f2594a5da79e741eb8fc1cadede
“[Krishnakant-pal]” <[krishnakant.pal127@gmail.com]>
2023-07-30 14:52:24 +0530

    first commit

--- app.py
+++ app.py
@@ -1,31 +1,43 @@
 from flask import Flask,request,render_template,jsonify
 
-
 app = Flask(__name__)
 
 
+dic = {"add":"+","subtract":"-","multiply":"*","divide": '/'}
+
 @app.route('/')
 def welcome():
-    return "<h>Welcome to the Flask</h>"
+    return render_template('welcome.html')
 
-@app.route('/calculator',methods = ["GET"])
-def math_operation():
+@app.route('/calculator')
+def index():
+    return render_template('calculate.html')
 
-    operation = request.json["operation"]
-    number1 = request.json["number1"]
-    number2 = request.json["number2"]
 
-    if operation == "add":
-        result = int(number1) + int(number2)
-    elif operation == "multiply":
-        result = int(number1) * int(number2)
-    elif operation == "division":
-        result = int(number1) / int(number2)
-    elif operation == "subtraction":
-        result = int(number1) - int(number2)
-    else:
-        result = "Please provide the proper operation"
-    return "The result is {}".format(result)        
+@app.route('/calculate',methods = ["POST","GET"])
+def math_operation():   
+    try: 
+    
+        num1 = float(request.form['num1'])
+        num2 = float(request.form['num2'])
+        operation = request.form['operation']
+
+        if operation == "add":
+            result = num1 + num2
+        elif operation == "subtract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "divide":
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                return "<h3>Error: Cannot divide by 0</h3>"
+
+        return render_template('result.html', num1=num1, num2=num2, operation=dic[operation], result=result)
+
+    except ValueError:
+        return "<h3>Error: Invalid input. Please enter valid numbers.</h3>"
 
 
 if __name__ == '__main__':
